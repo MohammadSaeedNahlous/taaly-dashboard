47686ea9e459844092e273cfc01f21ce1712bc43
Mohammad Saeed Nahlous <jay.haji041@gmail.com>
2024-06-14 10:00:54 +0200

    small fix

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon.svg" />
-        <link rel="stylesheet" href="bower_components/aos/dist/aos.css" />
-        <script src="bower_components/aos/dist/aos.js"></script>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@400..800&display=swap" rel="stylesheet" />
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,6 +4,16 @@ import { getDatabase, getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 
+// const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+// };
+
 const firebaseConfig = {
     apiKey: "AIzaSyD6JYtXLSmenp2XpAaLCB1PVy6IzQtixYs",
     authDomain: "taaly-firebase.firebaseapp.com",
